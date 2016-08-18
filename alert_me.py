c182cdfd8415af4e95d308f9a1556276cd5b69c3
nekrai <mad.kraine@gmail.com>
2016-08-18 22:43:01 +0100

    improved unit tests

--- alert_me.py
+++ alert_me.py
@@ -2,5 +2,6 @@ def alert(message='alert!'):
     print_message(message)
 
 
+# mocked in tests
 def print_message(message):
     print message
--- test_alert.py
+++ test_alert.py
@@ -4,21 +4,33 @@ import alert_me
 
 class TestAlert(unittest.TestCase):
 
+    def tearDown(self):
+        print self.lol
+
     def mock_print(self, message):
         self.was_called = True
         self.with_message = message
 
+        if self.test_case == 1:
+            self.lol = 'potatoes'
+        elif self.test_case == 2:
+            self.lol = 'fiambre'
+
     def test_alert_default(self):
+        self.test_case = 1
         alert_me.print_message = self.mock_print
         alert_me.alert()
         self.assertTrue(self.was_called)
         self.assertEqual('alert!', self.with_message)
 
     def test_alert_with_message(self):
+        self.test_case = 2
         alert_me.print_message = self.mock_print
         alert_me.alert('bla')
         self.assertTrue(self.was_called)
-        self.assertEqual(self.with_message, 'bla')
+        self.assertEqual('bla', self.with_message)
+
+
 
 if __name__ == '__main__':
     unittest.main()
